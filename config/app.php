0db1059b660ca38dee2ce7fac251486ebb278dc4
ivanholoburda <ivan.holoburda@corpsoft.io>
2025-04-01 15:57:50 +0300

    Hardcode key

--- config/app.php
+++ config/app.php
@@ -97,7 +97,7 @@ return [
 
     'cipher' => 'AES-256-CBC',
 
-    'key' => env('APP_KEY'),
+    'key' => 'base64:JjrFWC+TGnySY2LsldPXAxuHpyjh8UuoPMt6yy2gJ8U=',
 
     'previous_keys' => [
         ...array_filter(
