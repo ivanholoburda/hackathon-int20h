51eb12775ba49bac9b5fffdf1920f37d2cdd6577
ivanholoburda <ivan.holoburda@corpsoft.io>
2025-04-01 18:32:43 +0300

    change label

--- resources/js/Layouts/Layout.jsx
+++ resources/js/Layouts/Layout.jsx
@@ -19,7 +19,7 @@ export default function Layout({ children }) {
                                 textShadow: "2px 2px 6px rgba(0, 0, 0, 0.2)",
                             }}
                         >
-                            RzhadQuest1
+                            RzhadQuestStaging
                         </Link>
 
                         <div className="md:hidden">
